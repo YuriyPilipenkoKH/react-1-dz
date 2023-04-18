2030659502177ead80f2c12ee564ca729592391d
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-18 11:24:32 +0300

    go

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,17 @@
 
 import transactions from 'data/transactions.json'
 // import { Container } from './App.styled';
+import { Profile } from 'components/Profile/Profile';
 import { TransactionTable } from 'components/TransactionsHistory/TransactionHistory';
-
+import '../css/index.css'
+import user from '../data/user.json'
 
 export const App = () => {
   return (
+
     <div>
-   <TransactionTable transactions = {transactions} />
+      <Profile/>
+   {/* <TransactionTable transactions = {transactions} /> */}
     </div>
   );
 }; 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,11 @@
 import PropTypes from 'prop-types';
 
 export const Profile = () => {
-    
+    return (
+      <Profile className="profile">
+        Profile
+        {/* <Description></Description>
+<Stats></Stats> */}
+      </Profile>
+    );
 };
